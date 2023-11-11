e090dfdafec868ddcb12b0856bd381856dcd66ec
Deepak kumar <59889069+dth99@users.noreply.github.com>
2023-11-11 05:05:50 +0000

    factorial trailing zeroes efficient way

--- 01_mathematics/04_factorial_trailing_zeroes.md
+++ 01_mathematics/04_factorial_trailing_zeroes.md
@@ -1,1 +1,9 @@
+### the idea is to count how many 2, 5, we have in prime factorization in given in factorialization
+### interersting fact number of 5s are going to be less the no. of 2s
+
 ![dth99](https://github.com/dth99/dsa/blob/main/misc/Screenshot%202023-11-11%20101916.png)
+
+
+### after every 5 numbers, their is atleast one 5 factor 
+### [n/5]
+### 25 has has two 5 factors
--- 01_mathematics/04_factorial_trailing_zeros.cpp
+++ 01_mathematics/04_factorial_trailing_zeros.cpp
@@ -18,24 +18,18 @@ int trailingZeroes(int n) {
 // efficient way
 // the idea is to count how many 2, 5, we have in prime factorization in given in factorialization
 // interersting fact number of 5s are going to be less the no. of 2s
-int trailingZeroes(int n) {
+int trailingZeroes1(int n) {
 
         int zeroes=0;
-        int fact=1;
-        while(n!=0){
-            fact*=n;
-            n--;
-        }
-        while(fact%10==0){
-            fact/=10;
-            zeroes++;
+        for(int i=5;i<=n;i*=5){
+            zeroes+=(n/i);
         }
         return zeroes;
 }
 int main()
 {
-    int n=5;
-   cout<<trailingZeroes(n);
+    int n=100;
+   cout<<trailingZeroes1(n);
     
     return 0;
 }
