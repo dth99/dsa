aa02840aac6d2dcbe7854c29c46636a5c68ade1f
Deepak kumar <59889069+dth99@users.noreply.github.com>
2023-11-14 19:09:45 +0000

    file: ss

--- 01_mathematics/07_primeNumber.cpp
+++ 01_mathematics/07_primeNumber.cpp
@@ -17,9 +17,20 @@ bool isPrime1(int n){
     }
     return true;
 }
+
+// most efficient way
+bool isPrime2(int n){
+    if(n==1) return false;
+    if(n==2 || n==3) return true;
+    if(n%2==0 || n%3==0) return false;
+    for(int i=5;i*i<=n;i+6){
+        if(n%i==0 || n%(i+2)==0) return false;
+    }
+    return true;
+}
 int main()
 {
     int n=253;
-    cout<<isPrime(n);
+    cout<<isPrime2(n);
     return 0;
 }
